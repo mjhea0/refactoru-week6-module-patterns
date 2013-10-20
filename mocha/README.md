68cbf584ca28ee2e7dea16d5ce31f3b92dffe772
Michael Herman <michael@mherman.org>
2013-10-20 09:50:29 -0600

    initial

---
+++ mocha/README.md
@@ -0,0 +1,1 @@
+boilerplate for javascript/jquery tdd with mocha
---
+++ mocha/spec/specs.js
@@ -0,0 +1,5 @@
+describe('FUNCTION_NAME', function() {
+  it("adds 'ay' to the end of a word that starts with a vowel", function() {
+    FUNCTION_NAME('RESULT').should.equal('ANSWER');
+  });
+});
